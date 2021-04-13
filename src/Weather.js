6265f2306ba1162b377abaa1dbc1e2bc13a7a9ab
anjaporter88 <anjaporter88@users.noreply.github.com>
2021-04-13 07:59:31 +0100

    Update Weather.js
    - added return within function, still issue with "Unexpected token, expected "," (13:8)"

--- src/Weather.js
+++ src/Weather.js
@@ -2,15 +2,16 @@ import React from "react";
 import axios from 'axios';
 
 export default function Weather() {
-    function getInfo(response) {
-        let temp = response.data.main.temp;
+    return (
+        function getInfo(response) {
+            let temp = response.data.main.temp;
+            return (
+                alert (`The temperature in Brighton is ${Math.round(temp)}°C.`)
+            );
+        }
 
-        return (
-            alert (`The temperature in Brighton is ${Math.round(temp)}°C.`)
-        );
-    }
-
-    let apiKey = `3ac4d291dda277122b73faa304a1df6a`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brighton&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(getInfo);
+        let apiKey = `3ac4d291dda277122b73faa304a1df6a`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brighton&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(getInfo);
+    );
 }
